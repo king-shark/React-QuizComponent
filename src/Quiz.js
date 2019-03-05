966633ae6f3d3fd9d08d13b6dfd86b4132b14d16
Shark, Greg <Gregory.Shark@tdameritrade.com>
2019-03-05 11:50:21 -0600

    completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
         super(props)
         this.state = {quiz_position: 1}
     }
-    showNextQuestion = () => {
+    showNextQuestion() {
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
         })
